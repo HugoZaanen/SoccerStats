21f54a0c0b278722dc22db74668ffcc8d22527e3
Huego <hugozaanen@hotmail.com>
2019-01-21 18:25:39 +0100

    Soccerstats

--- SoccerStats/Program.cs
+++ SoccerStats/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SoccerStats
 {
@@ -10,6 +11,29 @@ namespace SoccerStats
     {
         static void Main(string[] args)
         {
+            string CurrentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(CurrentDirectory);
+            var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
+            var fileContents = ReadFile(fileName);
+            string[] fileLines = fileContents.Split(new char[] { '\r','\n'}, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in fileLines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine(fileContents);           
+        }
+
+        public static string ReadFile(string fileName)
+        {
+            using (var reader = new StreamReader(fileName))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        public static List<string> ReadSoccerResults(string fileName)
+        {
+                       
         }
     }
 }
